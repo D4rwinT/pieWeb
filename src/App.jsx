db467b5c8c506ee4561318e6701921e1aadb71d2
D4rwinT <163784163+D4rwinT@users.noreply.github.com>
2025-11-25 01:13:31 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import Tarjeta from './Tarjeta';
+
+function App() {
+  return (
+    <div>
+      
+      <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center mt-4">
+        Pie Review
+      </h1>
+      
+      <div class="overflow-x-auto snap-x flex flex-row items-center mt-4 p-6 bg-gray-100 text-center gap-8">
+        <Tarjeta 
+          imagen="img/pie3.jpg" 
+          titulo="Pie 2025"
+          descripcion="¡El mejor pie del año 2025!" 
+        />
+
+        <Tarjeta 
+          imagen="img/pie.jpg" 
+          titulo="Pie 2023" 
+          descripcion="Este pie anduvo medio flopero sinceramente, pero igual estaba rico. El detalle es que se sentía un poco falso." 
+        />
+
+        <Tarjeta 
+          imagen="img/pie2.jpg" 
+          titulo="Pie 2022" 
+          descripcion="¡El mejor pie del año 2022! Mis felicitaciones a la chef." 
+        />
+
+        <Tarjeta 
+          imagen="img/pie4.webp" 
+          titulo="Pie Unimarc" 
+          descripcion="No es muy bueno porque se le siente un sabor como artificial." 
+        />
+      </div>
+
+      <div class="flex flex-col items-center mt-4 mx-4 p-4 bg-gray-100 text-center">
+        <p>Mi <em>primera</em> <strong> Página Web</strong>.</p>
+        <p>Esta es una página HTML muy básica.</p>
+        <button class="mt-8 w-28 py-2 bg-gray-50 text-black rounded-lg hover:bg-blue-100 transition duration-300">
+          Haz clic acá
+        </button>
+        <button class="mt-6 w-40 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition duration-300">
+          Ver Perfil
+        </button> 
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Tarjeta.jsx
@@ -0,0 +1,28 @@
+function Tarjeta({ imagen, titulo, descripcion }) {
+  return (
+    <div className="ring-4 ring-gray-400 hover:ring-indigo-400 flex-shrink-0 relative group w-80 h-80 rounded-2xl overflow-hidden shadow-xl cursor-pointer">
+      
+      {/* 1. La Imagen Dinámica */}
+      <img 
+        src={imagen} 
+        alt={titulo} 
+        className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" 
+      />
+
+      {/* 2. El Texto Dinámico (Overlay) */}
+      <div className="absolute inset-0 bg-black/60 flex items-end justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div className="justify-center bg-gray-300 py-2 w-72 rounded-lg mb-2">
+          <h3 className="text-gray-700 text-2xl font-bold text-center px-2">
+          {titulo}
+          </h3>
+          <h4 className="text-gray-700 text-center px-2">
+            {descripcion}
+          </h4>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Tarjeta;
