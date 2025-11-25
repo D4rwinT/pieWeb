1f11e42830c4e0f652f3c9e74fb5ea98e7d1eaec
D4rwinT <163784163+D4rwinT@users.noreply.github.com>
2025-11-25 19:57:56 -0300

    Detalles estéticos: Ahora dice Recientes arriba y le agregue radientes a algunas cosas, aparte de más pies de ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ function App() {
   return (
     <div>
       
-      <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center mt-4">
+      <h1 className="text-4xl font-bold text-indigo-600 mb-4 text-center mt-4">
         Pie Review
       </h1>
       
-      <div class="overflow-x-auto snap-x flex flex-row items-center mt-4 p-6 bg-gray-100 text-center gap-8">
-        <Tarjeta 
+      {/* Caja que contiene todo*/}
+      <div className="relative mt-12">
+        {/* Etiqueta de Recientes */}
+        <div className="absolute top-0 left-8 -translate-y-1/2 bg-gradient-to-b from-indigo-400 to-indigo-600 text-white px-6 py-2 rounded-full shadow-lg font-bold text-2xl tracking-wide">
+          Recientes
+        </div>
+
+        {/* Carrusel */}
+        <div className="overflow-x-auto snap-x flex flex-row items-center pl-6 mt-4 pt-12 pb-8 bg-gradient-to-b from-gray-100 to-gray-200 text-center gap-8">
+          <Tarjeta 
           imagen="img/pie3.jpg" 
           titulo="Pie 2025"
           descripcion="¡El mejor pie del año 2025!" 
@@ -28,10 +36,23 @@ function App() {
         />
 
         <Tarjeta 
+          imagen="img/pie5.jpg" 
+          titulo="Pie Mayo 2025-2" 
+          descripcion="Muy caro y dulce, pero rico." 
+        />
+
+        <Tarjeta 
+          imagen="img/pie6.jpg" 
+          titulo="Pie Mayo 2025-1" 
+          descripcion="Me gustó con los pedacitos de limón arriba, pero muy caro." 
+        />
+
+        <Tarjeta 
           imagen="img/pie4.webp" 
           titulo="Pie Unimarc" 
           descripcion="No es muy bueno porque se le siente un sabor como artificial." 
         />
+        </div>
       </div>
 
       <div class="flex flex-col items-center mt-4 mx-4 p-4 bg-gray-100 text-center">
